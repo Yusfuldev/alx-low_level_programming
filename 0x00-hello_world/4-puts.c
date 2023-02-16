884090a60f2e1c842683f0cbeaf42cba4bd297fe
Yusfuldev <owokoniran93@gmail.com>
2023-02-16 15:13:58 -0800

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Main - entry point
+ *
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
