a70490d9643b8a7112397d4331589e1d6d88d6d6
Yusfuldev <owokoniran93@gmail.com>
2023-02-16 17:16:08 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Main - prints a string
+ *
+ * return: (int)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+return (int);
+}
