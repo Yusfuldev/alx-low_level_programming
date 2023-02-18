bfa04d0007c9cf9684d8c0a12baa8b508817e6ac
Yusfuldev <owokoniran93@gmail.com>
2023-02-17 16:54:09 -0800

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: 0
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 90; i++)
+	{
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
+		if (i!=89)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
