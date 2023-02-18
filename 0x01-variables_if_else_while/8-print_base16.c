31b94f493ff4b6ba63b4b11751ef730dd98d96f3
Yusfuldev <owokoniran93@gmail.com>
2023-02-17 16:05:09 -0800

    base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,10 +8,15 @@
 int main(void)
 {
 	int i;
+	char j;
 
-	for (i = 0; i <= 16; i++)
+	for (i = 0; i < 10; i++)
 	{
-		putchar(i % x  + '0');
+		putchar((i % 10) + '0');
+	}
+	for (j = 'a'; j <= 'f'; j++)
+	{
+		putchar(j);
 	}
 	putchar('\n');
 	return (0);
