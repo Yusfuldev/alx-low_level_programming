a8b9d05155abe541deb2e9b04ff421b543ff87ab
Yusfuldev <owokoniran93@gmail.com>
2023-02-23 00:58:11 -0800

    infinite loop

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+  * positive_or_negative - tests function that prints if integer is positive or negative
+  * @i: input parameter
+  * Return: 0
+  */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
+}
