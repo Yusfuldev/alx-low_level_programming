844b9025c0f5e7b381bd707081156381ec3b1b7c
Yusfuldev <owokoniran93@gmail.com>
2023-02-27 04:06:48 -0800

    0-98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  * reset_to_98 - assign a pointer to an integer as parameter.
+  * @n: address of int to print.
+  * Return: nothing.
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
