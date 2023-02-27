95840fbb166a925584dff0847302a7545b15b9bf
Yusfuldev <owokoniran93@gmail.com>
2023-02-27 05:18:47 -0800

    stringlength

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * _strlen  - prints length of a string
+  * @s: string's length to be printed.
+  * Return: len
+  */
+
+int _strlen(char *s)
+{
+	int l;
+	l = 0;
+	while (s[l] != '\0')
+		l++;
+
+	return (l);
+}
