f926b49189277e852a583fbde943059c3b5f642f
Yusfuldev <owokoniran93@gmail.com>
2023-03-01 05:28:46 -0800

    concat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * _strcat - concats to strings.
+  * @dest: 1st string
+  * @src: 2nd string
+  *
+  * Return: concatinated string
+  */
+
+char *_strcat(char *dest, char *src)
+{
+	int j, i = 0;
+	
+	while (dest[i] != '\0')
+		i++;
+	for (j = 0; src[j] != '\0'; j++, i++)
+		dest[i] = src[j];
+	return (dest);
+}
