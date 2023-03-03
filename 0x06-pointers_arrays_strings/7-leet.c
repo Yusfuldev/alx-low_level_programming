5de8ee21eca9f24778c5596ea81c5950aaafe5ca
Yusfuldev <owokoniran93@gmail.com>
2023-03-02 23:18:57 -0800

    encode

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,10 +9,10 @@
 char *leet(char *s)
 {
 	int  k, i = 0;
-	char sub[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'},
-	c[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char sub[10] = "4433007711",
+	c[10] = "aAeEoOtTlL";
 	
-	while (s[i])
+	while (s[i] != '\0')
 	{
 		for (k = 0; k < 10; k++)
 			if (s[i] == c[k])
