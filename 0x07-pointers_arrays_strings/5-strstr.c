020abdca3af4b17f0381f246683a31fdd5ee2c93
Yusfuldev <owokoniran93@gmail.com>
2023-03-06 16:13:14 -0800

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strstr - finds the 1st occurence of the substring in the string
+ * @haystack: string variable
+ * @needle: string variable to search for
+ * Return: pointer to the begining of located substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+char *n = needle, *h = haystack;
+
+	while (*haystack)
+	{
+		h = haystack;
+		needle = n;
+		while (*haystack == *needle)
+		{
+			haystack++;
+			needle++;
+		}
+
+		if (*needle == '\0')
+			return (haystack);
+		haystack = h + 1;
+	}
+	return (NULL);
+}	
