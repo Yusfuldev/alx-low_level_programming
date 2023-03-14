d29ee23263cabd10e7bf9d68ea3557fcce1ba9b6
Yusfuldev <owokoniran93@gmail.com>
2023-03-14 09:15:43 -0700

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,6 +22,12 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 	if (s1 == NULL)
 		s1 = "";
+	if (s1 == NULL && s2 == NULL)
+		return ("");
+	if (s1 == NULL && s2 != NULL)
+		return (s2);
+	if (s2 == NULL && s1 != NULL)
+		return (s1);
 	while (i < size)
 	{
 		if (i < size1)
