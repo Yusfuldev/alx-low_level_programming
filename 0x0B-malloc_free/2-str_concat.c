2695e4e66cc775b317d703e6ab7d6419c7c31d08
Yusfuldev <owokoniran93@gmail.com>
2023-03-14 08:51:57 -0700

    str_concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * str_concat - concats to strings
+ * @s1: string to concat
+ * @s2: sring to concat
+ * Return: a pointerto allocated memory
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *new_str;
+	int i = 0, size, size1, size2;
+
+	size1 = strlen(s1);
+	size2 =  strlen(s2);
+	size = size1 + size2;
+	new_str = malloc(sizeof(char) * size + 1);
+	if (s2 == NULL)
+		return (NULL);
+	if (s1 == NULL)
+		return (NULL);
+	while (i < size)
+	{
+		if (i < size1)
+		{
+			new_str[i] = *s1;
+			s1++;
+		}
+		else
+		{
+			new_str[i] = *s2;
+			s2++;
+		}
+		i++;
+	}
+	new_str[i] = '\0';
+	return (new_str);
+}
