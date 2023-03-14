45a878bac338dcdeb630538ed66589db28912bfe
Yusfuldev <owokoniran93@gmail.com>
2023-03-14 13:29:25 -0700

    alloc-grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,12 +14,14 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
-		free(array);
 	}
 
 	array = (int **)malloc(sizeof(int *) * height);
 	if (array == NULL)
+	{
 		return (NULL);
+		free(array);
+	}
 	for (; i < height; i++)
 	{
 		array[i] = (int *)malloc(sizeof(int) * width);
