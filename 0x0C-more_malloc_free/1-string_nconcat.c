f44bd2a9725dd671821629b7102ce142c0f001ac
Yusfuldev <owokoniran93@gmail.com>
2023-03-16 10:55:21 -0700

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,6 +6,7 @@
  * @s1: string character
  * @s2: string character
  * @n: number of char to concat
+ * Return: pointer to concat string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
