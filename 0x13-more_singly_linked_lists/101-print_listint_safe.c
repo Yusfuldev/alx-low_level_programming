27d8e51609a6cfe83d6c608f1d160febda8ac9a9
Yusfuldev <owokoniran93@gmail.com>
2023-04-03 16:02:22 -0700

    more singly linked lists

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints list
+ * @head: points to start of list
+ * Return: number of nodes in size_t or exit 98 on failure
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count = 0;
+
+	while (head)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		count++;
+
+		if (head > head->next)
+		{
+			head = head->next;
+		}
+		else
+		{
+			head = head->next;
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			break;
+		}
+	}
+	return (count);
+}
