c92efbfc08078a5e1fd23b5822a6d797e40c54ab
Yusfuldev <owokoniran93@gmail.com>
2023-04-05 06:06:44 -0700

    updated 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,36 +10,35 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-        unsigned int i = 0;
-        listint_t *new;
-        listint_t *temp;
+	unsigned int i = 0;
+	listint_t *new;
+	listint_t *temp;
 
-        if (head == NULL)
-                return (NULL);
-        if (idx == 0)
-        {
-                new = malloc(sizeof(listint_t));
-                if (!new)
-                        return (NULL);
-                new->n = n;
-                new->next = *head;
-                *head = new;
-                return (*head);
-        }
-        temp = *head;
-        while (i < idx - 1)
-        {
-                if (!temp)
-                        return (NULL);
-                temp = temp->next;
-                i++;
-        }
-        new = malloc(sizeof(listint_t));
-        if (!new)
-                return (NULL);
-        new->n = n;
-        new->next = temp->next;
-        temp->next = new;
-        return (new);
+	if (head == NULL)
+		return (NULL);
+	if (idx == 0)
+	{
+		new = malloc(sizeof(listint_t));
+		if (!new)
+			return (NULL);
+		new->n = n;
+		new->next = *head;
+		*head = new;
+		return (*head);
+	}
+	temp = *head;
+	while (i < idx - 1)
+	{
+		if (!temp)
+			return (NULL);
+		temp = temp->next;
+		i++;
+	}
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = temp->next;
+	temp->next = new;
+	return (new);
 }
-
