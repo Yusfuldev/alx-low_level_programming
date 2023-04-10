7def03e60c0f29691d5e514061e9ac72da89cfd8
Yusfuldev <owokoniran93@gmail.com>
2023-04-10 09:55:48 -0700

    create creare_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,13 +19,13 @@ int create_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 
-	len = strlen(text_content);
 
 	if (text_content == NULL)
 	{
 		close(fd);
 		return (1);
 	}
+	len = strlen(text_content);
 
 	w_val = write(fd, text_content, len);
 	if (w_val < 0)
