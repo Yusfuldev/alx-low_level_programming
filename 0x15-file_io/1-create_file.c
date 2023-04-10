adf5da5565a51088e38f890c383158559d358776
Yusfuldev <owokoniran93@gmail.com>
2023-04-10 09:49:50 -0700

    create creare_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, w_val;
-	size_t len = strlen(text_content) + 1;
+	size_t len = strlen(text_content);
 
 	if (filename == NULL)
 		return (-1);
