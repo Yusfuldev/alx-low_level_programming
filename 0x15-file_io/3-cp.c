f5436e4aee47c7d7cf8e3bede8538626ca8f4d12
Yusfuldev <owokoniran93@gmail.com>
2023-04-10 17:03:27 -0700

    create 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,13 +12,13 @@ int _close(int fd);
  * Return: 0 exit on failure.
  */
 
+
 int main(int argc, char *argv[])
 {
-	int src, dest;
-	ssize_t read_val, w_val;
-	char buff[1024];
+	char buffer[1024];
+	int bytes_read = 0, nr = 1, src= -1, dest = -1, error = 0;
 
-	if (argc < 3)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
@@ -27,51 +27,56 @@ int main(int argc, char *argv[])
 	src = open(argv[1], O_RDONLY);
 	if (src < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file \"%s\"\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (dest < 0)
+	dest = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	if (src < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to \"%s\"\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		_close(src);
 		exit(99);
 	}
 
-	read_val = read(src, buff, sizeof(buff));
-	while (read_val > 0)
+	while (nr)
 	{
-		w_val  = write(dest, buff, read_val);
-		if (w_val < 0)
+		nr = read(src, buffer, 1024);
+		if (nr < 0)
 		{
-			_close(dest);
-			dprintf(STDERR_FILENO, "Error: Can't write to \"%s\"\n", argv[2]);
-		}
-		read_val = read(src, buff, sizeof(buff));
-	}
-		if (argv[1] == NULL || read_val < 0)
-		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			_close(src);
-			dprintf(STDERR_FILENO, "Error: Can't read from file \"%s\"\n", argv[1]);
+			_close(dest);
+			exit(98);
 		}
-	close(src);
-	close(dest);
+		else if (nr == 0)
+			break;
+		bytes_read += nr;
+		error = write(src, buffer, nr);
+		if (error < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			_close(src);
+			_close(dest);
+			exit(99);
+		}
+	}
 	return (0);
 }
+
 /**
  * _close - closes a file and prints error.
  * @fd: Descriptor for closed file
  * Return: 1 on success, -1 on failure
  */
+
 int _close(int fd)
 {
+
 	int error;
 
 	error = close(fd);
 	if (error < 0)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
 	return (error);
 }
