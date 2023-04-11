ee29970c80c2456605c6bb32b553570c96d2cd86
Owokoniran Yusuf Adebayo <99514391+Yusfuldev@users.noreply.github.com>
2023-04-11 14:00:42 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,11 +3,6 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-/*
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/uio.h>
-*/
 #include <fcntl.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
